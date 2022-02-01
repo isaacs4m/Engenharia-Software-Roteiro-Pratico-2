2ad9bb95f666d98572d8df4f211f0c0d64b5d979
Isaac Lima <isaac.lima1@outlook.com>
2022-02-01 18:27:55 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -45,4 +45,11 @@ public class Movie {
 
     return thisAmount;
   }
+
+  public int getFrequentRenterPoints(int daysRented) {
+    // add bonus for a two day new release rental
+    if ((_priceCode == Movie.NEW_RELEASE) && daysRented > 1) return 2;
+  
+    return 1;
+  }
 }
